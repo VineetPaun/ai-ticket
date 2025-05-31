834927e36530b12897b1fc53060af717b4b0701d
VineetPaun <129723487+VineetPaun@users.noreply.github.com>
2025-05-31 12:09:08 +0530

    chore: initialize package.json with dependencies and devDependencies for backend setup

---
+++ models/ticket.js
@@ -0,0 +1,25 @@
+import mongoose from "mongoose"
+
+const ticketSchema = new mongoose.Schema({
+    title: String,
+    description: String,
+    status: {
+        type: String,
+        default: "TODO"
+    },
+    createdBy: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        default: null
+    },
+    priority: String,
+    deadline: Date,
+    helpfulNotes: String,
+    relatedNotes: [String],
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
+})
+
+export default mongoose.model("User", ticketSchema)
---
+++ models/user.js
@@ -0,0 +1,21 @@
+import mongoose from "mongoose"
+
+const userSchema = new mongoose.Schema({
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        default: "user",
+        enum: ["user", "moderator", "admin"]
+    },
+    skills: [String],
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
+})
+
+export default mongoose.model("User", userSchema)
